4195cd4528ef009d942bd34e363f9878a3846416
JasonIRL <JasonIRL@users.noreply.github.com>
2019-11-14 10:19:48 -0600

    quick and dirty

--- app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
-
-# Create extensions
-db = SQLAlchemy()
-csrf = CSRFProtect()
-migrate = Migrate()
